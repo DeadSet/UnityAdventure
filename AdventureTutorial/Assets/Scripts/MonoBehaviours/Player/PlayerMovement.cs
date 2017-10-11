47f4cc08d238be5bc82a8ffb1f66a582c57f1962
DeadSet <DeadSet@users.noreply.github.com>
2017-10-10 21:22:34 -0500

    Inventory Added
    https://youtu.be/9zXvIEvyZ_c?t=25m25s

--- AdventureTutorial/Assets/Scripts/MonoBehaviours/Player/PlayerMovement.cs
+++ AdventureTutorial/Assets/Scripts/MonoBehaviours/Player/PlayerMovement.cs
@@ -1,77 +1,82 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.AI;
+using UnityEngine.EventSystems;
 
-public class PlayerMovement : MonoBehaviour {
-
+public class PlayerMovement : MonoBehaviour
+{
     public Animator animator;
     public NavMeshAgent agent;
-    public float inputHoldDelay = 0.5f;
-    public float turnSpeedThreshold = 0.5f;
+    public SaveData playerSaveData;
+    public float turnSmoothing = 15f;
     public float speedDampTime = 0.1f;
     public float slowingSpeed = 0.175f;
-    public float turnSmoothing = 15f;
+    public float turnSpeedThreshold = 0.5f;
+    public float inputHoldDelay = 0.5f;
+    
 
-    private WaitForSeconds inputHoldWait;
+    private Interactable currentInteractable;
     private Vector3 destinationPosition;
+    private bool handleInput = true;
+    private WaitForSeconds inputHoldWait;
+
+
+    private readonly int hashSpeedPara = Animator.StringToHash("Speed");
+    private readonly int hashLocomotionTag = Animator.StringToHash("Locomotion");
+
+
+    public const string startingPositionKey = "starting position";
 
 
     private const float stopDistanceProportion = 0.1f;
     private const float navMeshSampleDistance = 4f;
-    private Interactable currentInteractable;
-    private bool handleInput = true;
-
-    private readonly int hashSpeedPara = Animator.StringToHash("Speed");
-    private readonly int hashLocomotionTag = Animator.StringToHash("Locomotion");
 
 
     private void Start()
     {
         agent.updateRotation = false;
 
-        inputHoldWait = new WaitForSeconds(inputHoldDelay);
+        inputHoldWait = new WaitForSeconds (inputHoldDelay);
+
+        string startingPositionName = "";
+        playerSaveData.Load(startingPositionKey, ref startingPositionName);
+        Transform startingPosition = StartingPosition.FindStartingPosition(startingPositionName);
+
+        transform.position = startingPosition.position;
+        transform.rotation = startingPosition.rotation;
 
         destinationPosition = transform.position;
-
-
     }
+
 
     private void OnAnimatorMove()
     {
-        agent.velocity = animator.deltaPosition / Time.deltaTime; 
+        agent.velocity = animator.deltaPosition / Time.deltaTime;
     }
+
 
     private void Update()
     {
         if (agent.pathPending)
-        {
             return;
-        }
 
         float speed = agent.desiredVelocity.magnitude;
-
+        
         if (agent.remainingDistance <= agent.stoppingDistance * stopDistanceProportion)
-        {
-            Stopping(out speed);
-        }
+            Stopping (out speed);
         else if (agent.remainingDistance <= agent.stoppingDistance)
-        {
             Slowing(out speed, agent.remainingDistance);
-        }
         else if (speed > turnSpeedThreshold)
-        {
-            Moving();
-        }
-
+            Moving ();
+        
         animator.SetFloat(hashSpeedPara, speed, speedDampTime, Time.deltaTime);
     }
 
 
     private void Stopping (out float speed)
     {
-        agent.isStopped = true;
+        agent.Stop();
+
         transform.position = destinationPosition;
         speed = 0f;
 
@@ -80,83 +85,79 @@ public class PlayerMovement : MonoBehaviour {
             transform.rotation = currentInteractable.interactionLocation.rotation;
             currentInteractable.Interact();
             currentInteractable = null;
-            StartCoroutine(WaitForInteraction());
-        }
 
+            StartCoroutine (WaitForInteraction ());
+        }
     }
 
-    private void Slowing(out float speed, float distanceToDestination)
-    {
-        agent.isStopped = true;
-        transform.position = Vector3.MoveTowards(transform.position, destinationPosition, slowingSpeed * Time.deltaTime);
 
-        float proportionalDistance = 1f - distanceToDestination/agent.stoppingDistance;
-        speed = Mathf.Lerp(slowingSpeed, 0f, proportionalDistance);
+    private void Slowing (out float speed, float distanceToDestination)
+    {
+        agent.Stop();
+
+        float proportionalDistance = 1f - distanceToDestination / agent.stoppingDistance;
 
         Quaternion targetRotation = currentInteractable ? currentInteractable.interactionLocation.rotation : transform.rotation;
-
         transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, proportionalDistance);
+
+        transform.position = Vector3.MoveTowards(transform.position, destinationPosition, slowingSpeed * Time.deltaTime);
+
+        speed = Mathf.Lerp(slowingSpeed, 0f, proportionalDistance);
     }
 
-    private void Moving()
+
+    private void Moving ()
     {
         Quaternion targetRotation = Quaternion.LookRotation(agent.desiredVelocity);
         transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turnSmoothing * Time.deltaTime);
     }
 
+
     public void OnGroundClick(BaseEventData data)
     {
-
         if(!handleInput)
-        {
             return;
-        }
-
+        
         currentInteractable = null;
 
         PointerEventData pData = (PointerEventData)data;
+
         NavMeshHit hit;
-        if (NavMesh.SamplePosition(pData.pointerCurrentRaycast.worldPosition, out hit, navMeshSampleDistance, NavMesh.AllAreas))
-        {
+        if (NavMesh.SamplePosition (pData.pointerCurrentRaycast.worldPosition, out hit, navMeshSampleDistance, NavMesh.AllAreas))
             destinationPosition = hit.position;
-        }
         else
-        {
             destinationPosition = pData.pointerCurrentRaycast.worldPosition;
-        }
 
         agent.SetDestination(destinationPosition);
-        agent.isStopped = false;
-
+        agent.Resume ();
     }
 
-    public void OnInteractableClick (Interactable interactable)
+
+    public void OnInteractableClick(Interactable interactable)
     {
-        if (!handleInput)
-        {
+        if(!handleInput)
             return;
-        }
 
         currentInteractable = interactable;
-        destinationPosition = currentInteractable.interactionLocation.position;
-        agent.SetDestination(destinationPosition);
-        agent.isStopped = false;
 
+        destinationPosition = currentInteractable.interactionLocation.position;
+
+        agent.SetDestination(destinationPosition);
+        agent.Resume ();
     }
 
-    private IEnumerator WaitForInteraction()
+
+    private IEnumerator WaitForInteraction ()
     {
         handleInput = false;
 
         yield return inputHoldWait;
 
-        while(animator.GetCurrentAnimatorStateInfo(0).tagHash != hashLocomotionTag)
+        while (animator.GetCurrentAnimatorStateInfo (0).tagHash != hashLocomotionTag)
         {
             yield return null;
         }
 
         handleInput = true;
-
     }
-
 }
